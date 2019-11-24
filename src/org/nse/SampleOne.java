faddd5a1071082107585ffb43ba1488322efed60
abineshm90 <abinesh.jaihind@gmail.com>
2019-11-24 20:21:18 +0530

    done

---
+++ src/org/nse/SampleOne.java
@@ -0,0 +1,16 @@
+package org.nse;
+
+public class SampleOne {
+	
+public void details() {
+	
+	System.out.println("details is new ");
+}	
+	public static void main(String[] args) {
+		SampleOne n = new SampleOne();
+		
+		n.details();
+	}
+
+
+}
