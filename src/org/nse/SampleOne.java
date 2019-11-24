b95865378579a91eddc1bfb26130087c9620bc99
abineshm90 <abinesh.jaihind@gmail.com>
2019-11-24 21:00:02 +0530

    welcome guys

--- src/org/nse/SampleOne.java
+++ src/org/nse/SampleOne.java
@@ -6,10 +6,17 @@ public void details() {
 	
 	System.out.println("details is new ");
 }	
+
+
+public void detailsOne() {
+	System.out.println("12345");
+
+}
 	public static void main(String[] args) {
 		SampleOne n = new SampleOne();
 		
 		n.details();
+		n.detailsOne();
 	}
 
 
